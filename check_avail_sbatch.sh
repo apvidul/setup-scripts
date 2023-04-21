a6df18af35af4cb8c50cf6dcc298217191f73e79
Vidul Ayakulangara Panickan <apvidul@gmail.com>
2023-04-21 14:02:10 -0400

    update

--- check_avail_sbatch.sh
+++ check_avail_sbatch.sh
@@ -9,7 +9,7 @@
 echo "STATUS CHECK START"
 
 while true; do
-    if sinfo  --Format=nodehost,available,memory,statelong,gres:40 -p gpu,gpu_quad,gpu_requeue | grep  "idle"| grep -q -i -E 'V100|RTX'; then
+    if sinfo  --Format=nodehost,available,memory,statelong,gres:40 -p gpu,gpu_quad,gpu_requeue | grep  "idle"| grep -q -i -E 'V100|a100'; then
          echo -e  "Time $(date)\n $(sinfo  --Format=nodehost,available,memory,statelong,gres:40 -p gpu,gpu_quad,gpu_requeue | grep "idle")" | mail -s "GPU AVAILABLE" your_email@gmail.com
          sleep 10
         break
--- check_avail_tmux.sh
+++ check_avail_tmux.sh
@@ -2,7 +2,7 @@
 echo "STATUS CHECK START"
 
 while true; do
-    if sinfo  --Format=nodehost,available,memory,statelong,gres:40 -p gpu,gpu_quad,gpu_requeue | grep  "idle"| grep -q -i -E 'v100|RTX'; then
+    if sinfo  --Format=nodehost,available,memory,statelong,gres:40 -p gpu,gpu_quad,gpu_requeue | grep  "idle"| grep -q -i -E 'V100|a100'; then
          echo -e  "Time $(date)\n $(sinfo  --Format=nodehost,available,memory,statelong,gres:40 -p gpu,gpu_quad,gpu_requeue | grep "idle")" | mail -s "GPU AVAILABLE" apvidul@gmail.com
          sleep 10
         break
